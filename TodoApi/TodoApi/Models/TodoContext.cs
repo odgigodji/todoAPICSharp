f8df8a3a320300ba9a89602650812735f75331a6
odgigodji <AZM-snn-J9f-G2L>
2021-10-20 22:41:01 +0300

    add Models folder

---
+++ TodoApi/TodoApi/Models/TodoContext.cs
@@ -0,0 +1,20 @@
+﻿/*
+    Контекст базы данных —это основной класс, который координирует функциональные
+    возможности Entity Framework для модели данных.
+    Этот класс является производным от класса Microsoft.EntityFrameworkCore.DbContext.
+*/
+
+using Microsoft.EntityFrameworkCore;
+
+namespace TodoApi.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
