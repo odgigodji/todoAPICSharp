1e8f6a0df42d7a4f19adfc3c3ecdd396e13dbade
Nelson Amerei <namerei@si-m5.kzn.21-school.ru>
2021-10-22 15:09:35 +0300

    date work

--- TodoApi/TodoApi/Models/TodoItem.cs
+++ TodoApi/TodoApi/Models/TodoItem.cs
@@ -6,9 +6,19 @@ namespace TodoApi.Models
     {
         public long Id { get; set; } //identifier
         public string Type { get; set; }
-        public string Descriprion { get; set; }
-        // public bool IsComplete { get; set; }
-        public DateTime DateOfCompletion { get; set; }
-        
+        public string Description { get; set; }
+        // public DateTime DateOfCompletion { get; set; }
+
+        // public DateTime Date
+        // {
+        //     get { return Date; }
+        //     set
+        //     {
+        //         // Date = DateTime.Now;
+        //         // Console.WriteLine(Date);
+        //         var dateAndTime = Date;
+        //         Date = dateAndTime.Date;
+        //     }
+        // } 
     }
 }
